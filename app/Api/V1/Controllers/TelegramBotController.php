38b34a73cd824cdcccf3cbcfeb98fee63dbb0676
chinedu117 <chinedudimonyeka@gmail.com>
2022-03-20 13:04:56 +0100

    Route test ok

---
+++ app/Api/V1/Controllers/TelegramBotController.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Controller;
+
+class TelegramBotController extends Controller {
+   
+
+    public function sendTestMessage(){
+         
+        return "ok";
+        
+    }
+
+}
